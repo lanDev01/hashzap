9fae5574ac4b80d8ac2119956aff4e6a6af9027e
LanDev01 <alnjr0001@outlook.com>
2024-01-12 09:45:07 -0300

    creating popup

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import flet as ft
 
 def main(page):
-    text = ft.Text("HashZap")
+    text = ft.Text("HashZap") # Titulo da pagina
 
-    def startChat(event):
-        print("Iniciar o chat")
+    nameUser = ft.TextField(label="Escreva seu nome")
 
+    popUp = ft.AlertDialog(
+        open=False,
+        modal=True, 
+        title=ft.Text("Bem vindo ao HashZap"), 
+        content=nameUser,
+        actions=[ft.ElevatedButton("Entrar")]
+    ) # criando PopUp
+
+    def startChat(event): # Função que faz o pop aparecer
+        page.dialog = popUp
+        popUp.open = True
+        page.update()
     button = ft.ElevatedButton("Iniciar Chat", on_click=startChat)
 
 

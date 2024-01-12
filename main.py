c6e102606b9afffff84fc9dc142a77e62eb622f6
LanDev01 <alnjr0001@outlook.com>
2024-01-12 09:29:45 -0300

    creating the initialize chat button

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import flet as ft
 def main(page):
     text = ft.Text("HashZap")
 
+    def startChat(event):
+        print("Iniciar o chat")
+
+    button = ft.ElevatedButton("Iniciar Chat", on_click=startChat)
+
+
     page.add(text)
+    page.add(button)
     
 ft.app(main)
